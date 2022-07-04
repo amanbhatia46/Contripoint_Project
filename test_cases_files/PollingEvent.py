fcc458f63929aa59597bc07b2e66eefb0da1868f
amanbhatia46 <aman.bhatia@geminisolutions.com>
2022-07-04 17:00:44 +0530

    type

--- test_cases_files/PollingEvent.py
+++ test_cases_files/PollingEvent.py
@@ -365,6 +365,30 @@ class Login(unittest.TestCase):
             self.ws['G10'] = 'Fail'
             self.wb.save(self.filename)
 
+    def Reward(self):
+        """
+
+        """
+        try:
+            # Entering Reward
+            self.driver.find_element(
+                By.ID, "mat-input-4").send_keys("Cash Rs1000/-")
+            time.sleep(5)
+            print("\n 18 - Entering Reward  ")
+
+            self.ws['C11'] = 'Reward '
+            self.ws['G11'] = 'Pass'
+            self.wb.save(self.filename)
+
+        except Exception as e:
+            print("\n ERROR IN Reward  >>>>>>>>>>>>>>>\n\n")
+            print(e)
+            print("\n 18 - Unable to Add Reward ")
+
+            self.ws['C11'] = 'Reward '
+            self.ws['G11'] = 'Fail'
+            self.wb.save(self.filename)
+
     def tearDown(self):
         self.driver.quit()
 
@@ -382,4 +406,5 @@ if __name__ == '__main__':
     tb.Event_Name()
     tb.Start_Date()
     tb.End_Date()
-    
+    tb.Reward()
+
--- test_cases_files/try.py
+++ test_cases_files/try.py
@@ -1,26 +1,23 @@
-def End_Date(self):
+def Reward(self):
         """
 
         """
         try:
-            # Clicking on End Date
+            # Entering Reward
             self.driver.find_element(
-                By.XPATH, '/html/body/div[2]/div[2]/div/mat-dialog-container/app-event-modal/mat-dialog-content/mat-horizontal-stepper/div[2]/div[2]/form/div[2]/div[2]/mat-form-field/div/div[1]/div[1]/input').click()
+                By.ID, "mat-input-4").send_keys("Cash Rs1000/-")
             time.sleep(5)
-            print("\n 13 - Clicking on End Date Calander ")
+            print("\n 18 - Entering Reward  ")
 
-            self.driver.find_element(By.XPATH,'//*[@id="mat-datepicker-0"]/div/mat-month-view/table/tbody/tr[2]/td[2]/div[1]').click()
-            time.sleep(5)
-
-            self.ws['C7'] = 'End Date'
-            self.ws['G7'] = 'Pass'
+            self.ws['C11'] = 'Reward '
+            self.ws['G11'] = 'Pass'
             self.wb.save(self.filename)
 
         except Exception as e:
-            print("\n ERROR IN End Date  >>>>>>>>>>>>>>>\n\n")
+            print("\n ERROR IN Reward  >>>>>>>>>>>>>>>\n\n")
             print(e)
-            print("\n 13 - Unable to Add End Date ")
+            print("\n 18 - Unable to Add Reward ")
 
-            self.ws['C7'] = 'End Date'
-            self.ws['G7'] = 'Fail'
+            self.ws['C11'] = 'Reward '
+            self.ws['G11'] = 'Fail'
             self.wb.save(self.filename)
